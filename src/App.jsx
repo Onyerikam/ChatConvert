de486e6b2931ec37f121ea748dc093183d169567
Onyerikam <andrespedrionyerikam@gmail.com>
2023-01-29 20:06:57 +0100

    Update or no update code for life

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState } from "react";
+import ChatRoom from "./components/ChatRoom";
+import ChatMessage from "./components/ChatMessage";
+import EmailConverter from "./components/EmailConverter";
+import GroupChat from "./components/GroupChat";
+import Chatbot from "./components/Chatbot";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentView, setCurrentView] = useState("chatroom");
+  const [email, setEmail] = useState("");
+  const [groupName, setGroupName] = useState("");
+  const [groupMembers, setGroupMembers] = useState([]);
+  const [messages, setMessages] = useState([]);
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handleGroupNameChange = (event) => {
+    setGroupName(event.target.value);
+  };
+
+  const handleGroupMembersChange = (event) => {
+    setGroupMembers(event.target.value);
+  };
+
+  const handleSendMessage = (message) => {
+    setMessages([...messages, message]);
+  };
+
+  const handleCreateGroup = () => {
+    setCurrentView("groupchat");
+  };
+
+  const handleBackToChatroom = () => {
+    setCurrentView("chatroom");
+  };
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {currentView === "chatroom" && (
+        <ChatRoom
+          email={email}
+          onEmailChange={handleEmailChange}
+          onCreateGroup={handleCreateGroup}
+        />
+      )}
+      {currentView === "groupchat" && (
+        <GroupChat
+          groupName={groupName}
+          groupMembers={groupMembers}
+          onGroupNameChange={handleGroupNameChange}
+          onGroupMembersChange={handleGroupMembersChange}
+          onBackToChatroom={handleBackToChatroom}
+        />
+      )}
+      {currentView === "emailconverter" && (
+        <EmailConverter email={email} />
+      )}
+      {currentView === "chatmessage" && (
+        <ChatMessage messages={messages} onSendMessage={handleSendMessage} />
+      )}
+      {currentView === "chatbot" && <Chatbot />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
